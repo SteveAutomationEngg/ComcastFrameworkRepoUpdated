92156f8036c77d351109de7c7bd32a4662cdd4ea
MOHIT KUMAR <MOHIT KUMAR@Spidy>
2024-09-23 16:06:47 +0530

    just simple push

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/basetest/BaseClass.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/basetest/BaseClass.java
@@ -11,9 +11,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
+
 import com.comcast.crm.generic.databaseutility.DataBaseUtility;
 import com.comcast.crm.generic.fileutility.ExcelUtility;
 import com.comcast.crm.generic.fileutility.FileUtility;
